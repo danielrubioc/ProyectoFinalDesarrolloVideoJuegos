3037e690d9282ff7f85b2336a3d139febe9d979e
Sergio Ávila <sergio.avila.astorga@gmail.com>
2023-02-28 23:11:06 -0300

    clean animation script

--- Assets/Scripts/Player/Animation.cs
+++ Assets/Scripts/Player/Animation.cs
@@ -6,12 +6,6 @@ public class Animation : MonoBehaviour
 {
     public Animator anim;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
     void Update()
     {
         // get player component
